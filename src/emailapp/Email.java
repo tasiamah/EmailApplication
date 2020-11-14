80dec2801b0111b9c45f3877dfd039181ac8943b
Ted <ted.a@live.nl>
2020-11-14 19:57:26 +0100

    first commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,16 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String alternateEmail;
+    private int inbox;
+
+    // Constructer to receive the first name and last name
+
+    // Ask for the department
+
+    //
+}
